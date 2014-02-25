aa4adfeb121d52bf863215bcd384a7996b382766
creatory <info@creatory.net>
2014-02-25 06:54:44 -0500

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '5885c08e6d69eb95086786a8dbf28eb226c26c53600f17e62e2cf8c31433b85066873f8d29f5a87df57bae05a6c5a65f840267ba10bdccbec94d37243c474342'
